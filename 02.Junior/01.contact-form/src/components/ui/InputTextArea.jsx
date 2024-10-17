94989e54d241dc8fcfd15ded85a67a7a52fbb38e
roid zuhdianto <roidzuh@gmail.com>
2024-10-17 09:53:51 +0700

    update size rows text area

--- 02.Junior/01.contact-form/src/components/ui/InputTextArea.jsx
+++ 02.Junior/01.contact-form/src/components/ui/InputTextArea.jsx
@@ -11,8 +11,7 @@ const InputTextArea = ({ onChange, value, error, onBlur }) => {
         <textarea
           name="message"
           id="message"
-          rows="8"
-          className={`border border-grey500 rounded-lg py-3 px-6 text-body-md-regular text-grey900 resize-none focus:outline-none transition-all duration-300 cursor-pointer ${
+          className={`border border-grey500 rounded-lg py-3 px-6 text-body-md-regular text-grey900 resize-none focus:outline-none transition-all duration-300 cursor-pointer h-60 md:h-32 ${
             error
               ? 'border-redError'
               : 'hover:border-green600 focus:border-green600'
