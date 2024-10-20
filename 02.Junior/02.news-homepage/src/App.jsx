5b60d70930ba75808b2b9edf3d31659c219ae765
roid zuhdianto <roidzuh@gmail.com>
2024-10-20 11:24:34 +0700

    update

--- 02.Junior/02.news-homepage/src/App.jsx
+++ 02.Junior/02.news-homepage/src/App.jsx
@@ -4,13 +4,32 @@ import RelatePost from './components/layout/RelatePost';
 
 const App = () => {
   return (
-    <div className="mt-7 mx-4 mb-20 lg:mt-[89px] lg:mb-[129px]">
-      <Header />
-      <main>
-        <MainNews />
-        <RelatePost />
-      </main>
-    </div>
+    <>
+      <div className="mt-7 mx-4 mb-20 lg:mt-[89px] lg:mb-[129px]">
+        <Header />
+        <main>
+          <MainNews />
+          <RelatePost />
+        </main>
+      </div>
+      <p className="text-center mb-4">
+        Challenge by{' '}
+        <a
+          href="https://www.frontendmentor.io"
+          className="text-Light-Vermillion underline"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{' '}
+        <a
+          href="https://www.frontendmentor.io/profile/roidzuh"
+          className="text-Light-Vermillion underline"
+        >
+          Roid Zuhdianto
+        </a>
+        .
+      </p>
+    </>
   );
 };
 
