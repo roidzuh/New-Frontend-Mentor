af20603ecbf1fe5fbad4c10f1fb97da43c56facb
roid zuhdianto <roidzuh@gmail.com>
2024-10-20 10:36:00 +0700

    change aria roles

--- 02.Junior/02.news-homepage/src/components/layout/MainNews.jsx
+++ 02.Junior/02.news-homepage/src/components/layout/MainNews.jsx
@@ -5,7 +5,7 @@ const MainNews = () => {
   return (
     <section
       className="flex flex-col mt-8 lg:mt-[55px] lg:flex-row lg:gap-[30px] lg:max-w-[1200px] lg:mx-auto "
-      role="main-news"
+      role="main"
     >
       <Article />
       <NewsList />
